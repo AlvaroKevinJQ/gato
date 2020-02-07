34ae4c852d0f4103395f0d052055acf5a640d674
Kevin <altairx90@gmail.com>
2020-02-06 18:58:54 -0600

    Agregando el juego del gato

---
+++ GATO/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace GATO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PB2 = new System.Windows.Forms.PictureBox();
+            this.PB3 = new System.Windows.Forms.PictureBox();
+            this.PB4 = new System.Windows.Forms.PictureBox();
+            this.PB5 = new System.Windows.Forms.PictureBox();
+            this.PB6 = new System.Windows.Forms.PictureBox();
+            this.PB7 = new System.Windows.Forms.PictureBox();
+            this.PB8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.PB1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 458);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // PB2
+            // 
+            this.PB2.Location = new System.Drawing.Point(307, 35);
+            this.PB2.Name = "PB2";
+            this.PB2.Size = new System.Drawing.Size(169, 86);
+            this.PB2.TabIndex = 2;
+            this.PB2.TabStop = false;
+            this.PB2.Click += new System.EventHandler(this.PB2_Click);
+            // 
+            // PB3
+            // 
+            this.PB3.Location = new System.Drawing.Point(664, 35);
+            this.PB3.Name = "PB3";
+            this.PB3.Size = new System.Drawing.Size(136, 86);
+            this.PB3.TabIndex = 3;
+            this.PB3.TabStop = false;
+            this.PB3.Click += new System.EventHandler(this.PB3_Click);
+            // 
+            // PB4
+            // 
+            this.PB4.Location = new System.Drawing.Point(12, 235);
+            this.PB4.Name = "PB4";
+            this.PB4.Size = new System.Drawing.Size(123, 100);
+            this.PB4.TabIndex = 4;
+            this.PB4.TabStop = false;
+            this.PB4.Click += new System.EventHandler(this.PB4_Click);
+            // 
+            // PB5
+            // 
+            this.PB5.Location = new System.Drawing.Point(307, 235);
+            this.PB5.Name = "PB5";
+            this.PB5.Size = new System.Drawing.Size(169, 100);
+            this.PB5.TabIndex = 5;
+            this.PB5.TabStop = false;
+            this.PB5.Click += new System.EventHandler(this.PB5_Click);
+            // 
+            // PB6
+            // 
+            this.PB6.Location = new System.Drawing.Point(664, 235);
+            this.PB6.Name = "PB6";
+            this.PB6.Size = new System.Drawing.Size(136, 88);
+            this.PB6.TabIndex = 6;
+            this.PB6.TabStop = false;
+            this.PB6.Click += new System.EventHandler(this.PB6_Click);
+            // 
+            // PB7
+            // 
+            this.PB7.Location = new System.Drawing.Point(12, 382);
+            this.PB7.Name = "PB7";
+            this.PB7.Size = new System.Drawing.Size(123, 76);
+            this.PB7.TabIndex = 7;
+            this.PB7.TabStop = false;
+            this.PB7.Click += new System.EventHandler(this.PB7_Click);
+            // 
+            // PB8
+            // 
+            this.PB8.Location = new System.Drawing.Point(307, 382);
+            this.PB8.Name = "PB8";
+            this.PB8.Size = new System.Drawing.Size(169, 76);
+            this.PB8.TabIndex = 8;
+            this.PB8.TabStop = false;
+            this.PB8.Click += new System.EventHandler(this.PB8_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(664, 385);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(136, 73);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // PB1
+            // 
+            this.PB1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PB1.Location = new System.Drawing.Point(12, 48);
+            this.PB1.Name = "PB1";
+            this.PB1.Size = new System.Drawing.Size(123, 86);
+            this.PB1.TabIndex = 10;
+            this.PB1.TabStop = false;
+            this.PB1.Click += new System.EventHandler(this.PB1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 453);
+            this.Controls.Add(this.PB1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.PB8);
+            this.Controls.Add(this.PB7);
+            this.Controls.Add(this.PB6);
+            this.Controls.Add(this.PB5);
+            this.Controls.Add(this.PB4);
+            this.Controls.Add(this.PB3);
+            this.Controls.Add(this.PB2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox PB2;
+        private System.Windows.Forms.PictureBox PB3;
+        private System.Windows.Forms.PictureBox PB4;
+        private System.Windows.Forms.PictureBox PB5;
+        private System.Windows.Forms.PictureBox PB6;
+        private System.Windows.Forms.PictureBox PB7;
+        private System.Windows.Forms.PictureBox PB8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox PB1;
+
+    }
+}
+
---
+++ GATO/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GATO
+{
+    public partial class Form1 : Form
+    {
+        public int contG = 2, c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0, c7 = 0, c8 = 0, c9 = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void cargarX(PictureBox obj)
+        {
+            // You should replace the bold image   
+            // in the sample below with an icon of your own choosing.  
+            // Note the escape character used (@) when specifying the path.  
+            obj.Image = Image.FromFile("C:\\Users\\altai\\source\\repos\\GATO\\GATO\\Recursos\\xd.png");
+           
+        }
+      
+        void ganador()
+        {
+            if ((c1 == 1) & (c2 == 1) & (c3 == 1)) {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c1 == 2) & (c2 == 2) & (c3 == 2))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+            if ((c1 == 1) & (c4 == 1) & (c7 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c1 == 1) & (c4 == 1) & (c7 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+            if ((c1 == 1) & (c5 == 1) & (c9 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c1 == 2) & (c5 == 2) & (c9 == 2))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+            if ((c4 == 1) & (c5 == 1) & (c6 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c4 == 2) & (c5 == 2) & (c6 == 2))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+            if ((c7 == 1) & (c8 == 1) & (c9 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c7 == 2) & (c8 == 2) & (c9 == 2))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+            if ((c2 == 1) & (c5 == 1) & (c8 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c2 == 2) & (c5 == 2) & (c8 == 2))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+            if ((c3 == 1) & (c6 == 1) & (c9 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c3 == 2) & (c6 == 2) & (c9 == 2))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+            if ((c3 == 1) & (c5 == 1) & (c7 == 1))
+            {
+                Console.WriteLine("GANO EL JUGADOR 1");
+            }
+            if ((c3 == 2) & (c5 == 2) & (c7 == 2))
+            {
+                Console.WriteLine("GANO EL JUGADOR 2");
+            }
+        }
+
+
+
+        void cargarY(PictureBox obj)
+        {
+
+            obj.Image = Image.FromFile("C:\\Users\\altai\\source\\repos\\GATO\\GATO\\Recursos\\c.png");
+
+        }
+
+            private void PB1_Click(object sender, EventArgs e) //PRIMER BOTON
+        {
+            
+            if (contG % 2 == 0)
+            {
+                cargarX(PB1);
+                c1 = 1;
+
+            }
+            else
+            {
+                cargarY(PB1);
+                c1 = 2;
+            }
+            contG++;
+            ganador();
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) //MAPA
+        {
+
+        }
+
+       
+
+        private void PB2_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(PB2);
+                c2 = 1;
+            }
+            else
+            {
+                cargarY(PB2);
+                c2 = 2;
+            }
+            contG++;
+            ganador();
+
+        }
+
+        private void PB3_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(PB3);
+                c3 = 1;
+            }
+            else
+            {
+                cargarY(PB3);
+                c3 = 4;
+            }
+            contG++;
+            ganador();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PB4_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(PB4);
+                c4 = 1;
+            }
+            else
+            {
+                cargarY(PB4);
+                c4 = 2;
+            }
+            contG++;
+            ganador();
+        }
+
+        private void PB5_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(PB5);
+                c5 = 1;
+            }
+            else
+            {
+                cargarY(PB5);
+                c5 = 2;
+            }
+            contG++;
+            ganador();
+        }
+
+        private void PB6_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(PB6);
+                c6 = 1;
+            }
+            else
+            {
+                cargarY(PB6);
+                c6 = 2;
+            }
+            contG++;
+            ganador();
+        }
+
+        private void PB7_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(PB7);
+                c7 = 1;
+            }
+            else
+            {
+                cargarY(PB7);
+                c7 = 2;
+            }
+            contG++;
+            ganador();
+        }
+
+        private void PB8_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(PB8);
+                c8 = 1;
+            }
+            else
+            {
+                cargarY(PB8);
+                c8 = 2;
+            }
+            contG++;
+            ganador();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (contG % 2 == 0)
+            {
+                cargarX(pictureBox2);
+                c9 = 1;
+            }
+            else
+            {
+                cargarY(pictureBox2);
+                c9 = 2;
+            }
+            contG++;
+            ganador();
+        }
+    }
+}
